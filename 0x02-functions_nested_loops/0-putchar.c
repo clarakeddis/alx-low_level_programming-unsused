7c633468325d21188f0b3ec89531715d2c78e1fa
root <root@DESKTOP-9VVVEBU.localdomain>
2024-06-26 01:13:54 +0300

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - prints _putchar followed by a new line
+ *
+ *Retaurn: Always 0 (sucess)
+ */
+ int main(void)
+ {
+    int i;
+    for (i=0; i<8; i++)
+ }
+{
+    putchar("_putchar"[i]);
+}
+putchar('\n');
+return (0); }
